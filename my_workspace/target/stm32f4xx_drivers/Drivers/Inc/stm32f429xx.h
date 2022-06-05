22c97aa322b2448902b462cfb253b0b2f82ac44c
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2022-06-04 23:06:55 -0500

    Section 20: Begin working on STM32F429xx drivers
            - Begin work on the stm32f429xx header file

---
+++ my_workspace/target/stm32f4xx_drivers/Drivers/Inc/stm32f429xx.h
@@ -0,0 +1,15 @@
+/**
+ * @file stm32f429xx.h
+ * @author Michael Ly (github.com/Michael-Q-Ly)
+ * @brief 
+ * @version 0.1
+ * @date 2022-06-04
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef _STM32F29XX_H_
+#define _STM32F29XX_H_
+
+
+#endif /* _STM32F29XX_H_ */
