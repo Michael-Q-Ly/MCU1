2dbc9b8515144ff870c2011ee61c20262a73e959
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2022-06-05 00:23:25 -0500

    Section 20: Add #defines for FLASH and SRAM in MCU's header file

--- my_workspace/target/stm32f4xx_drivers/Drivers/Inc/stm32f429xx.h
+++ my_workspace/target/stm32f4xx_drivers/Drivers/Inc/stm32f429xx.h
@@ -11,5 +11,18 @@
 #ifndef _STM32F29XX_H_
 #define _STM32F29XX_H_
 
+/*
+ * Base addresses of FLASH and SRAM memories
+ */
+
+#define ROM                     0x1FFF0000UL
+#define FLASH_BASE_ADDR         0x80000000UL
+#define SRAM1_BASE_ADDR         0x20000000UL
+#define SRAM1_SIZE              0x1C000U                                /**<112 kB */
+#define SRAM2_BASE_ADDR         (SRAM1_BASE_ADDR + SRAM1_SIZE)
+#define SRAM2_SIZE              0x4000U                                 /* 16 kB */
+#define SRAM3_BASE_ADDR         (SRAM2_BASE_ADDR + SRAM2_SIZE)
+#define SRAM3_SIZE              0x10000U                                /* 64 kB */
+#define SRAM                    SRAM1_BASE_ADDR
 
 #endif /* _STM32F29XX_H_ */
