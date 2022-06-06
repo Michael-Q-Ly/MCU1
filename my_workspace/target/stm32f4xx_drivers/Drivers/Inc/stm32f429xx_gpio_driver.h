8dacb385317c1534d83a0dfeb0be0b3fad5af659
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2022-06-06 01:42:00 +0100

    Section 23: Begin working on GPIO driver API
            - Create source and header files for GPIO APIO

---
+++ my_workspace/target/stm32f4xx_drivers/Drivers/Inc/stm32f429xx_gpio_driver.h
@@ -0,0 +1,16 @@
+/**
+ * @file stm32f429xx_gpio_driver.h
+ * @author Michael Ly (github.com/Michael-Q-Ly)
+ * @brief 
+ * @version 0.1
+ * @date 2022-06-05
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef _STM32F429XX_H_
+#define _STM32F429XX_H_
+
+#include "stm32f429xx.h"
+
+#endif /* _STM32F429XX_H_ */
---
+++ my_workspace/target/stm32f4xx_drivers/Drivers/Src/stm32f429xx_gpio_driver.c
@@ -0,0 +1,11 @@
+/**
+ * @file stm32f429xx_gpio_driver.c
+ * @author Michael Ly (github.com/Michael-Q-Ly)
+ * @brief 
+ * @version 0.1
+ * @date 2022-06-05
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include "stm32f429xx_gpio_driver.h"
