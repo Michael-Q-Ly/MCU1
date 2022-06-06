e1f7425a3a3d8667874cc1b587d2cb9227109025
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2022-06-06 16:09:22 -0500

    Section 26: Edit project settings to include source files in Driver directory

--- my_workspace/target/stm32f4xx_drivers/Debug/Src/subdir.mk
+++ my_workspace/target/stm32f4xx_drivers/Debug/Src/subdir.mk
@@ -5,26 +5,26 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/main.c \
+../Src/001_LedToggle.c \
 ../Src/sysmem.c 
 
 OBJS += \
-./Src/main.o \
+./Src/001_LedToggle.o \
 ./Src/sysmem.o 
 
 C_DEPS += \
-./Src/main.d \
+./Src/001_LedToggle.d \
 ./Src/sysmem.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 Src/%.o: ../Src/%.c Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32F429I_DISC1 -DSTM32 -DSTM32F429ZITx -DSTM32F4 -c -I"C:/Users/Micha/OneDrive/Documents/Embedded_C/MCU1/my_workspace/target/stm32f4xx_drivers/Drivers/Inc" -I"C:/Users/Micha/OneDrive/Documents/Embedded_C/MCU1/my_workspace/target/stm32f4xx_drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g -DDEBUG -DSTM32F429I_DISC1 -DSTM32 -DSTM32F429ZITx -DSTM32F4 -c -I../Inc -I"C:/Users/Micha/OneDrive/Documents/Embedded_C/MCU1/my_workspace/target/stm32f4xx_drivers/Drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 
 clean: clean-Src
 
 clean-Src:
-	-$(RM) ./Src/main.d ./Src/main.o ./Src/sysmem.d ./Src/sysmem.o
+	-$(RM) ./Src/001_LedToggle.d ./Src/001_LedToggle.o ./Src/sysmem.d ./Src/sysmem.o
 
 .PHONY: clean-Src
 
