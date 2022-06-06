19cefd429e7e9d22e6efbc8c494630e601f1ea74
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2022-06-06 16:08:13 -0500

    Section 26: Fix GPIOx_REG_RESET macro typo for J and K

--- my_workspace/target/stm32f4xx_drivers/Drivers/Inc/stm32f429xx.h
+++ my_workspace/target/stm32f4xx_drivers/Drivers/Inc/stm32f429xx.h
@@ -331,16 +331,16 @@ typedef struct {
  * Macros to reset GPIOx peripherals
  */
 
-#define GPIOA_REG_RESET()       do {(RCC->AHB1RSTR |= BIT0) ;   (RCC->AHB1RSTR &= ~BIT0) ;} while(0)
-#define GPIOB_REG_RESET()       do {(RCC->AHB1RSTR |= BIT1) ;   (RCC->AHB1RSTR &= ~BIT1) ;} while(0)
-#define GPIOC_REG_RESET()       do {(RCC->AHB1RSTR |= BIT2) ;   (RCC->AHB1RSTR &= ~BIT2) ;} while(0)
-#define GPIOD_REG_RESET()       do {(RCC->AHB1RSTR |= BIT3) ;   (RCC->AHB1RSTR &= ~BIT3) ;} while(0)
-#define GPIOE_REG_RESET()       do {(RCC->AHB1RSTR |= BIT4) ;   (RCC->AHB1RSTR &= ~BIT4) ;} while(0)
-#define GPIOF_REG_RESET()       do {(RCC->AHB1RSTR |= BIT5) ;   (RCC->AHB1RSTR &= ~BIT5) ;} while(0)
-#define GPIOG_REG_RESET()       do {(RCC->AHB1RSTR |= BIT6) ;   (RCC->AHB1RSTR &= ~BIT6) ;} while(0)
-#define GPIOH_REG_RESET()       do {(RCC->AHB1RSTR |= BIT7) ;   (RCC->AHB1RSTR &= ~BIT7) ;} while(0)
-#define GPIOI_REG_RESET()       do {(RCC->AHB1RSTR |= BIT8) ;   (RCC->AHB1RSTR &= ~BIT8) ;} while(0)
-#define GPIO_JREG_RESET()       do {(RCC->AHB1RSTR |= BIT9) ;   (RCC->AHB1RSTR &= ~BIT9) ;} while(0)
-#define GPIO_KREG_RESET()       do {(RCC->AHB1RSTR |= BIT10) ;   (RCC->AHB1RSTR &= ~BIT10) ;} while(0)
+#define GPIOA_REG_RESET()       do {    (RCC->AHB1RSTR |= BIT0) ;   (RCC->AHB1RSTR &= ~BIT0) ;  } while(0)
+#define GPIOB_REG_RESET()       do {    (RCC->AHB1RSTR |= BIT1) ;   (RCC->AHB1RSTR &= ~BIT1) ;  } while(0)
+#define GPIOC_REG_RESET()       do {    (RCC->AHB1RSTR |= BIT2) ;   (RCC->AHB1RSTR &= ~BIT2) ;  } while(0)
+#define GPIOD_REG_RESET()       do {    (RCC->AHB1RSTR |= BIT3) ;   (RCC->AHB1RSTR &= ~BIT3) ;  } while(0)
+#define GPIOE_REG_RESET()       do {    (RCC->AHB1RSTR |= BIT4) ;   (RCC->AHB1RSTR &= ~BIT4) ;  } while(0)
+#define GPIOF_REG_RESET()       do {    (RCC->AHB1RSTR |= BIT5) ;   (RCC->AHB1RSTR &= ~BIT5) ;  } while(0)
+#define GPIOG_REG_RESET()       do {    (RCC->AHB1RSTR |= BIT6) ;   (RCC->AHB1RSTR &= ~BIT6) ;  } while(0)
+#define GPIOH_REG_RESET()       do {    (RCC->AHB1RSTR |= BIT7) ;   (RCC->AHB1RSTR &= ~BIT7) ;  } while(0)
+#define GPIOI_REG_RESET()       do {    (RCC->AHB1RSTR |= BIT8) ;   (RCC->AHB1RSTR &= ~BIT8) ;  } while(0)
+#define GPIOJ_REG_RESET()       do {    (RCC->AHB1RSTR |= BIT9) ;   (RCC->AHB1RSTR &= ~BIT9) ;  } while(0)
+#define GPIOK_REG_RESET()       do {    (RCC->AHB1RSTR |= BIT10) ;  (RCC->AHB1RSTR &= ~BIT10) ;} while(0)
 
 #endif /* _STM32F29XX_H_ */
