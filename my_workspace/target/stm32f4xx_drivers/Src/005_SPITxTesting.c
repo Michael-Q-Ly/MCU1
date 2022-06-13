871f0b9f41587f57f92d649d4c3a5c69326ba41b
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2022-06-13 02:23:05 -0500

    Section 40: Add init functions to SPI2 TX test program
            - SPI2_GPIOInits
            - SPI2_Inits

--- my_workspace/target/stm32f4xx_drivers/Src/005_SPITxTesting.c
+++ my_workspace/target/stm32f4xx_drivers/Src/005_SPITxTesting.c
@@ -19,6 +19,71 @@
 #include "stm32f429xx_gpio_driver.h"
 #include "stm32f429xx_spi_driver.h"
 
+void SPI2_GPIOInits(void) ;
+void SPI2_inits(void) ;
+
 int main(void) {
+    SPI2_GPIOInits() ;
     return 0 ;
+}
+
+/****************************************************************************************************
+ * @fn                  SPI2_GPIOInits
+ * 
+ * @brief               Initialize the GPIO pins to behave as SPI2 pins
+ * 
+ * @return              none
+ * 
+ * @note                none
+ * 
+ */
+void SPI2_GPIOInits(void) {
+    /*
+     * Configure the SPI pins
+     */
+
+    GPIO_Handle_t SPIPins ;
+    SPIPins.pGPIOx = GPIOB ;
+    SPIPins.GPIO_PinConfig.GPIO_PinMode         = GPIO_MODE_ALT_FUN ;
+    SPIPins.GPIO_PinConfig.GPIO_PinSpeed        = GPIO_SPEED_FAST ;
+    SPIPins.GPIO_PinConfig.GPIO_PinPuPdCtrl     = GPIO_NO_PUPD ;
+    SPIPins.GPIO_PinConfig.GPIO_PinOPType       = GPIO_OP_TYPE_PP ;
+    SPIPins.GPIO_PinConfig.GPIO_PinAltFunMode   = 5 ;
+
+
+    /*
+     * Configure the SPI2 pins
+     */
+
+    // NSS
+    SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12 ;
+    GPIO_Init(&SPIPins) ;
+    // SCK
+    SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_13 ;
+    GPIO_Init(&SPIPins) ;
+    // MISO
+    SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_14 ;
+    GPIO_Init(&SPIPins) ;
+    // MOSI
+    SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_15 ;
+    GPIO_Init(&SPIPins) ;
+}
+
+/**
+ * @brief Initializes SPI2 peripheral
+ * 
+ */
+void SPI2_inits(void) {
+    SPI_Handle_t SPI2Handle ;
+
+    SPI2Handle.pSPIx = SPI2 ;
+    SPI2Handle.SPIConfig.SPI_DeviceMode     = SPI_DEVICE_MODE_CONTROLLER ;
+    SPI2Handle.SPIConfig.SPI_BusConfig      = SPI_BUS_CONFIG_FD ;
+    SPI2Handle.SPIConfig.SPI_SclkSpeed      = SPI_SCLK_SPEED_DIV2 ;                     /* Generates SCK of 8 MHz */
+    SPI2Handle.SPIConfig.SPI_DFF            = SPI_DFF_8BITS ;
+    SPI2Handle.SPIConfig.SPI_CPOL           = SPI_CPOL_LOW ;
+    SPI2Handle.SPIConfig.SPI_CPHA           = SPI_CPHA_LOW ;
+    SPI2Handle.SPIConfig.SPI_SSM            = SPI_SSM_EN ;                              /* Software slave management enabled for NSS pin */
+
+    SPI_Init(&SPI2Handle) ;
 }
